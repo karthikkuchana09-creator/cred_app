0cfad30b4a6a6a0a9e1df75482cdb2fc4c9cc856
karthik <karthikkuchana09@gmail.com>
2026-04-02 16:08:44 +0530

    addded comment

--- django_project/users/apps.py
+++ django_project/users/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    name = 'users' # This is the name of the app, which should match the name of the directory containing this file.
